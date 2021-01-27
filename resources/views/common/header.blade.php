c3a751fb1c1c576720f6945f069c05758a56bb48
Unknown <taikothman1997@gmail.com>
2021-01-27 11:26:31 +0100

    laravel-boolean

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <div class="news container ">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class=" col-12 d-flex align-items-center justify-content-center">
+                <marquee
+                        class='d-flex  align-items-center'
+                        loop="-1"
+                        behavior='scrool'
+                        scrollamount="3"
+                        scrolldelay="5"
+                        direction="left"
+                        height="30"
+                        width="1920"
+                        align="center">
+                        <p><i class="fa fa-bell" aria-hidden="true"></i> PROSSIMO CORSO IN PARTENZA: <span> 10 MARZO </span></p>
+                </marquee>
+            </div>
+        </div>
+    </div>
+    <nav class="">
+        <div class="row ">
+            <div class="col-3 logo d-flex align-items-center">
+                <img src="img/logo.png" alt="logo boolean">
+            </div>
+            <div class="col-9 navigation d-flex align-items-center justify-content-end ">
+                    {{-- inserire un ciclo foreach che permette di generare le varie pagine
+                        del sito e gestire la classe active su a  --}}
+                @foreach (config('menu.pages') as $page)
+                        <li class="{{ Route::currentRouteName() == $page['pathId'] ? 'active' : '' }}">
+                            <a href="{{route($page['pathId'])}}">{{$page['displaytext']}}</a>
+                        </li>
+                @endforeach 
+                   <div class="btn d-flex align-items-center justify-content-center"> <a href="">Candidati Ora</a></div>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.boolean')
+@section('jumbo')
+    <div class="container jumbo ">
+        <div class="row ">
+            <div class="col-6 d-flex flex-column align-items-start justify-content-center">
+                <h1>Cambia la tua vita. <br> Entra in Boolean.</h1>
+                <h4>Segui il corso, diventi un web <br> developer e trovi lavoro.</h4>
+                <li class="d-flex align-items-center justify-content-baseline">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <span>6 mesi di corso full time, online e in diretta</span>
+                </li>
+                <li class="d-flex align-items-center justify-content-baseline">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <span>Nessuna competenza di programmazione richiesta</span>
+                </li>
+                <li class="d-flex align-items-center justify-content-baseline">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <span>Se non trovi lavoro ti rimborsiamo</span>
+                </li>
+                <div class="btn header_btn d-flex align-items-center justify-content-center"> <a href="">Candidati Ora</a></div>
+            </div>
+            <div class="col-6 d-flex align-items-center justify-content-center">
+                <img src="img/homeHeader.gif" alt="">
+            </div>
+        </div>
+    </div>
+
+
+    
+@endsection
